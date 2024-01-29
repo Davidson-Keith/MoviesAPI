1c3858e818a0fedd74afc3e1ada1f148682031db
Keith Davidson <davidson.keith@gmail.com>
2024-01-30 07:22:23 +1000

    Messing about with some changes

--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -6,28 +6,27 @@ using MoviesAPI.Filters;
 namespace MoviesAPI {
   public class Startup {
     public IConfiguration Configuration { get; }
-    
+
     public Startup(IConfiguration configuration) {
       Configuration = configuration;
     }
-    
+
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
       services.AddDbContext<ApplicationDbContext>(options => {
         options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")); // From appsettings.*.json
       });
-      
-      services.AddControllers(options => {
-        options.Filters.Add(typeof(MyExceptionFilter));
-      });
+
+      services.AddControllers(options => { options.Filters.Add(typeof(MyExceptionFilter)); });
       services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
-      
+
       // services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "MoviesAPI", Version = "v1" }); });
 
       services.AddCors(options => {
+        var frontendUrl = Configuration.GetValue<string>("frontend_url");
         options.AddDefaultPolicy(builder => {
-          var frontendUrl = Configuration.GetValue<string>("frontend_url");
-          builder.WithOrigins(frontendUrl).AllowAnyMethod().AllowAnyHeader();
+          builder.WithOrigins(frontendUrl ?? throw new InvalidOperationException("frontendUrl for CORS is null."))
+            .AllowAnyMethod().AllowAnyHeader();
         });
       });
 
@@ -37,18 +36,17 @@ namespace MoviesAPI {
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger) {
       if (env.IsDevelopment()) {
-        // app.UseDeveloperExceptionPage();
-        // app.UseSwagger();
-        // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesAPI v1"));
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MoviesAPI v1"));
       }
+
       app.UseHttpsRedirection();
       app.UseRouting();
       app.UseCors();
       app.UseAuthentication();
       app.UseAuthorization();
-      app.UseEndpoints(endpoints => {
-        endpoints.MapControllers();
-      });
+      app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
     }
   }
 }
