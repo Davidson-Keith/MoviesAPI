36cf5ef9cdefadc9b2cfcd486c448cba91c5d02e
Keith Davidson <davidson.keith@gmail.com>
2024-10-23 14:49:08 +1000

    Change Required ErrorMessage to be more consistent with built in validation attribute error messages.

--- MoviesAPI/DTOs/GenreCreationDto.cs
+++ MoviesAPI/DTOs/GenreCreationDto.cs
@@ -6,10 +6,11 @@ namespace MoviesAPI.DTOs;
 /**
  * This is the data transferred from the user via a POST request to create a new Genre row in the DB. 
  * Validations are for testing the user input data.
- * Failed validation returns an error to the front end form???
+ * Failed validation returns an 400 Bad Request result to whatever called the POST request, with the validation errors in the
+ * result body
  */
 public class GenreCreationDto {
-  [Required(ErrorMessage = "The field with name {0} is required.")]
+  [Required(ErrorMessage = "The field {0} is required.")]
   // [StringLength(10)] // maxlength
   // [StringLength(maximumLength: 50, MinimumLength = 2)] // max + min length. Note the bizarre inconsistencies!
   [StringLength(maximumLength: 5, MinimumLength = 4)] // test BadRequestBehavior
