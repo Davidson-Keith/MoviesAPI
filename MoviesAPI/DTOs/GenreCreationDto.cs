ceada1f53a2d3d1c538a153dfef7a6c2163f7caa
Keith Davidson <davidson.keith@gmail.com>
2024-10-24 19:47:12 +1000

    Playing around with validations

--- MoviesAPI/DTOs/GenreCreationDto.cs
+++ MoviesAPI/DTOs/GenreCreationDto.cs
@@ -6,14 +6,14 @@ namespace MoviesAPI.DTOs;
 /**
  * This is the data transferred from the user via a POST request to create a new Genre row in the DB. 
  * Validations are for testing the user input data.
- * Failed validation returns an 400 Bad Request result to whatever called the POST request, with the validation errors in the
- * result body
+ * Failed validation returns an 400 Bad Request result to whatever called the POST request, with the validation errors
+ * in the result body
  */
 public class GenreCreationDto {
   [Required(ErrorMessage = "The field {0} is required.")]
   // [StringLength(10)] // maxlength
-  // [StringLength(maximumLength: 50, MinimumLength = 2)] // max + min length. Note the bizarre inconsistencies!
-  [StringLength(maximumLength: 5, MinimumLength = 4)] // test BadRequestBehavior
+  // [StringLength(maximumLength: 5, MinimumLength = 2)] // test BadRequestBehavior
+  [StringLength(maximumLength: 50, MinimumLength = 2)] // max + min length. Note the bizarre inconsistencies!
   [FirstLetterUppercase] // Our custom MoviesAPI.Validations.FirstLetterUppercaseAttribute validation
   public string Name { get; set; }
 }
--- MoviesAPI/Entities/Genre.cs
+++ MoviesAPI/Entities/Genre.cs
@@ -7,17 +7,32 @@ namespace MoviesAPI.Entities {
    * This is the raw data type stored in the DB.
    * Validations are for the DB requirements???
    */
+  // public class Genre : IValidatableObject {
   public class Genre {
     public int Id { get; set; }
 
-    // Validations moved to the Dto's
-    // All these are ignored on a POST request??? I can set min/max length here to whatever, and it never fails.
-    // Instead the validations for the GenreCreationDto are performed.
+    // Validations exist in the Dto's
+    // All the validatons here are ignored on a POST request!
+    // I can make these conditions fail all I like, but they never fail.
+    // The validations for the GenreCreationDto are performed if I have them, but not these.
+    // This is weird, as they work in his vids, but maybe as the course progressed, there was a change made that
+    // disabled validations here???  Oh, maybe it is the actual addition of the Dto's that did it. 
     // The DB itself only has a restriction that the column is nvarchar(50)
+
     // [Required(ErrorMessage = "The field with name {0} is required.")]
     // [StringLength(10)] // maxlength
     // [StringLength(maximumLength: 3, MinimumLength = 2)] // max + min length. Note the bizarre inconsistencies!
     // [FirstLetterUppercase] // Our custom MoviesAPI.Validations.FirstLetterUppercaseAttribute validation
     public string Name { get; set; }
+
+    // This doesn't work either. 
+    // public IEnumerable<ValidationResult> Validate(ValidationContext validationContext) {
+    //   if (!string.IsNullOrEmpty(Name)) {
+    //     var firstLetter = Name[0].ToString();
+    //     if (firstLetter != firstLetter.ToUpper()) {
+    //       yield return new ValidationResult("First letter should be uppercase", new string[] { nameof(Name) });
+    //     }
+    //   }
+    // }
   }
 }
